687076989d8591a2ff887c33efba1a4dde782837
Brandon Collins <brandonc503@yahoo.com>
2024-09-25 12:42:13 -0400

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        
+        response = self.get_response_body()
+        return json.loads(response)
+
+
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+#=> [{"name"=>"Daniel", "occupation"=>"LG Fridge Salesman"}, {"name"=>"Joe", "occupation"=>"WiFi Fixer"}, {"name"=>"Avi", "occupation"=>"DJ"}, {"name"=>"Howard", "occupation"=>"Mountain Legend"}]
+print(get_requester.load_json())
